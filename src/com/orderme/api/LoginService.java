c47f6015d2d4f8e178d9fa708654e9aae675908d
Hoang Nguyen <Hoang.NguyenMinh1@harveynash.vn>
2016-10-24 19:18:43 +0700

    Hoang: add Hoang

--- src/com/orderme/api/LoginService.java
+++ src/com/orderme/api/LoginService.java
@@ -4,13 +4,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/login")
 public class LoginService {
 	
 	@GET
 	public Response getMsg() {
 
-		String output = "Jersey say : ";
+		String output = "Jersey say : Hoang";
 
 		return Response.status(200).entity(output).build();
 

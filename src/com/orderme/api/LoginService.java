3f8cff1c3e5251c2361d4a4353a067edafa7889e
Hoang Nguyen <Hoang.NguyenMinh1@harveynash.vn>
2016-10-24 20:29:16 +0700

    Demo JSON

--- src/com/orderme/api/LoginService.java
+++ src/com/orderme/api/LoginService.java
@@ -1,18 +1,25 @@
 package com.orderme.api;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.orderme.entity.User;
 
 @Path("/login")
 public class LoginService {
 	
 	@GET
-	public Response getMsg() {
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getMsg() {
 
 		String output = "Jersey say : Hoang";
-
-		return Response.status(200).entity(output).build();
+		User u = new User();
+		u.setName("name");
+		u.setPassword("pass");
+		return u;
 
 	}
 }

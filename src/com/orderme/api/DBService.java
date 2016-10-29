8c5f83b836a306ea9c22f1fab37edc45502f0d60
Hoang Nguyen <Hoang.NguyenMinh1@harveynash.vn>
2016-10-29 15:01:27 +0700

    up

---
+++ src/com/orderme/api/DBService.java
@@ -0,0 +1,17 @@
+package com.orderme.api;
+
+import java.util.logging.Logger;
+
+public class DBService {
+
+	private static final Logger logger = Logger.getLogger("DBService");
+	
+	static {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			logger.severe("Cant load driver for JDBC");
+		}
+	}
+}
--- src/com/orderme/api/LoginService.java
+++ src/com/orderme/api/LoginService.java
@@ -1,25 +1,57 @@
 package com.orderme.api;
 
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.orderme.entity.Role;
 import com.orderme.entity.User;
 
-@Path("/login")
-public class LoginService {
+
+
+@Path("/user")
+public class LoginService extends DBService {
 	
-	@GET
-	//@PathParam()
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String login() throws ClassNotFoundException {
+	public String login(User user) throws ClassNotFoundException {
+		
+		EntityManagerFactory entityManagerFactory =  Persistence.createEntityManagerFactory("testjpa");
+	    EntityManager em = entityManagerFactory.createEntityManager();
+	    EntityTransaction userTransaction = em.getTransaction();
+	    
+	    String select = "SELECT us FROM orderme_user us WHERE us.email=:email and us.password=:password";
+	    Query query = em.createQuery(select);
+	    query.setParameter("userName", user.getEmail());
+	    query.setParameter("password", user.getPassword());
+	    List<User> users = query.getResultList();
+	    
+	    userTransaction.commit();
+	    em.close();
+	    entityManagerFactory.close();
+		return "call me hehe";
+
+	}
+	
+	@POST
+	//@PathParam()
+	@Path("/register")
+	@Produces(MediaType.APPLICATION_JSON)
+	public int register() throws ClassNotFoundException {
 		Class.forName("org.postgresql.Driver");
 		EntityManagerFactory entityManagerFactory =  Persistence.createEntityManagerFactory("testjpa");
 	    EntityManager em = entityManagerFactory.createEntityManager();
@@ -36,7 +68,7 @@ public class LoginService {
 	    userTransaction.commit();
 	    em.close();
 	    entityManagerFactory.close();
-		return "call me hehe";
+		return 1;
 
 	}
 }
